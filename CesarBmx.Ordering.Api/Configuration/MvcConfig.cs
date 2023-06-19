0f40dbc26f09ddd3f699a8e2d680c26878d61d61
Cesar C <cesarbmx@hotmail.com>
2023-06-19 18:08:29 +0200

    bool enableRazorPages = false

--- CesarBmx.Ordering.Api/Configuration/MvcConfig.cs
+++ CesarBmx.Ordering.Api/Configuration/MvcConfig.cs
@@ -6,14 +6,14 @@ namespace CesarBmx.Ordering.Api.Configuration
     {
         public static IServiceCollection ConfigureMvc(this IServiceCollection services, IConfiguration configuration)
         {
-            services.ConfigureSharedMvc(configuration, false);
+            services.ConfigureSharedMvc(configuration);
             
             return services;
         }
 
         public static IApplicationBuilder ConfigureMvc(this IApplicationBuilder app, IConfiguration configuration)
         {
-            app.ConfigureSharedMvc(configuration, false);
+            app.ConfigureSharedMvc(configuration);
 
             return app;
         }
